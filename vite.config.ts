b2726442daec9ddf6e906a47072f5586b8535971
Ishita Gupta <159607368+ixhita12@users.noreply.github.com>
2026-08-08 18:53:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    prerender: {
-      enabled: true,
-      crawlLinks: true,
-    },
-
-    server: { entry: "server" },
-  },
+  server: { entry: "server" },
+},
 });
